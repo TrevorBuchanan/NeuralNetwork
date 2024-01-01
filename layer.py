44023c7beb9934f8b5c73bc76903533693539743
TrevorBuchanan <123215971+TrevorBuchanan@users.noreply.github.com>
2024-01-01 00:17:35 -0800

    Cleaning up Layer class

--- layer.py
+++ layer.py
@@ -1,28 +1,29 @@
-# import numpy as np
+import numpy as np
 import math
+import random
 
 
 def activation_function(weighted_input):
-    return weighted_input
-    # return 1 / (1 + (math.e ** -weighted_input))
+    # return weighted_input
+    if weighted_input > 500:
+        return 1
+    if weighted_input < -500:
+        return 0
+
+    return 1 / (1 + (math.e ** -weighted_input))
 
 
 class Layer:
-    def __init__(self, inputs, output_amount):
-        self.inputs = inputs
-
-        self.numInputs = len(inputs)
+    def __init__(self, input_amount, output_amount):
+        self.numInputs = input_amount
         self.numOutputs = output_amount
 
-        # self.weights = np.random.rand(input_amount, output_amount)
-        self.weights = [[1 for _ in range(output_amount)] for _ in range(self.numInputs)]
-        # self.biases = np.random.rand(output_amount)
-        # self.biases = np.zeros(output_amount)
+        self.weights = [[random.uniform(-1, 1) / input_amount ** 0.5 for _ in range(output_amount)] for _ in
+                        range(input_amount)]
         self.biases = [0 for _ in range(output_amount)]
 
-
     # Compute outputs
-    def get_outputs(self, inputs):
+    def get_layer_outputs(self, inputs):
         activations = [0 for _ in range(self.numOutputs)]
         for out_index in range(self.numOutputs):
             weighted_input = self.biases[out_index]
