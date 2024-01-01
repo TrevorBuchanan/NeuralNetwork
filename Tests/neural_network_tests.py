9ccce3e423b0f741083149a7e8c52c1b1db3047b
TrevorBuchanan <123215971+TrevorBuchanan@users.noreply.github.com>
2024-01-01 00:18:28 -0800

    Adding neural_network_tests.py and its content

---
+++ Tests/neural_network_tests.py
@@ -0,0 +1,24 @@
+import math
+import unittest
+
+from neuralnetwork import NeuralNet
+
+
+class NeuralNetworkTests(unittest.TestCase):
+    def test_get_outputs(self):
+        inputs = [5, -2]
+        test_nn = NeuralNet([2, 3, 2])
+        weights1 = [[2, -2, -3], [4, 1, 0]]
+        biases1 = [3, -4, 5]
+        weights2 = [[7, -1], [3, 3], [-2, -6]]
+        biases2 = [-2, 3]
+        test_nn.layers[0].weights = weights1
+        test_nn.layers[0].biases = biases1
+        test_nn.layers[1].weights = weights2
+        test_nn.layers[1].biases = biases2
+
+        calc_out = test_nn.get_outputs(inputs)
+        exp_out = [0.9929877491461381, 0.8814695743502968]
+
+        self.assertEqual(exp_out[0], calc_out[0])
+        self.assertEqual(exp_out[1], calc_out[1])
