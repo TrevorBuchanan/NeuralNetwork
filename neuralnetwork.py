18d177eea70abe3f3b7b772554ede4384cf58aa1
TrevorBuchanan <123215971+TrevorBuchanan@users.noreply.github.com>
2024-01-01 00:21:37 -0800

    Re-organizing NeuralNetwork class and fixing logic errors

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,25 +1,41 @@
 import pygame
 
-from colors import RED, BLUE
+from colors import RED, BLUE, LIGHTER_GRAY
 from layer import Layer
 from settings import SCREEN, BORDER
 
 
-def create_layers(layer_sizes):
-    # # Initializes the first layer of NN according to input
-    # layers = [Layer(inputs, layer_sizes[0])]
-
-    layers = []
-    # Initializes the rest of the nodes in the NN to 0
-    for i in range(len(layer_sizes) - 1):
-        new_inputs = [0 for _ in range(layer_sizes[i])]
-        layers.append(Layer(new_inputs, layer_sizes[i + 1]))
-    return layers
-
-
 class NeuralNet:
     def __init__(self, layer_sizes):
-        self.layers = create_layers(layer_sizes)
+        self.layers = self.create_layers(layer_sizes)
+
+    @staticmethod
+    def normalize_inputs(inputs):
+        return inputs[0] / BORDER.width, inputs[1] / BORDER.height
+
+    def get_outputs(self, inputs):
+        if inputs[0] == 1000 and inputs[1] == 790:
+            pass
+        inputs = self.normalize_inputs(inputs)
+        if self.layers[0].numInputs == len(inputs):
+            for one_layer in self.layers:
+                inputs = one_layer.get_layer_outputs(inputs)
+        else:
+            raise Exception("Incorrectly sized input for neural network")
+        return inputs
+
+    @staticmethod
+    def create_layers(layer_sizes):
+        """
+        Creates layers according to layer sizes
+        :param layer_sizes: List of integers defining the layer sizes
+        :return: A List of Layers
+        """
+        layers = []
+        # Loop through layers and add layer to layers list
+        for i in range(len(layer_sizes) - 1):
+            layers.append(Layer(layer_sizes[i], layer_sizes[i + 1]))
+        return layers
 
     # Temp Functions
     def manual_set_weights(self, weights):  # Only when 2 inputs and 2 outputs
@@ -30,32 +46,25 @@ class NeuralNet:
                 i += 1
                 weight[1] = weights[i]
                 i += 1
+                if i <= 6:
+                    weight[2] = weights[i]
+                    i += 1
 
     def manual_set_biases(self, biases):
+        i = 0
         for layer in self.layers:
-            layer.biases[0] = biases[0]
-            layer.biases[1] = biases[1]
+            layer.biases[0] = biases[i]
+            i += 1
+            layer.biases[1] = biases[i]
+            i += 1
+            if i <= 2:
+                layer.biases[2] = biases[i]
+                i += 1
 
     def classify(self, inputs):
-        if len(self.layers[0].inputs) == len(inputs):
-            self.layers[0].inputs = inputs
-        else:
-            raise Exception("Incorrectly sized input for neural network")
-
-        for one_layer in self.layers:
-            inputs = one_layer.get_outputs(inputs)
-        outputs = inputs
+        outputs = self.get_outputs(inputs)
 
         if outputs[0] > outputs[1]:
-            return 1
-        else:
             return 0
-
-    def get_decision_boundary(self):
-        points = []
-        for x in range(0, BORDER.width, 10):
-            for y in range(0, BORDER.height, 1):
-                if self.classify((x, y)) != self.classify((x, y + 1)):
-                    points.append((x, y))
-        print()
-        return points
+        else:
+            return 1
