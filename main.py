b45984df501f82c3c2bcc5246d53d26bbd122be3
TrevorBuchanan <123215971+TrevorBuchanan@users.noreply.github.com>
2024-01-02 00:15:31 -0800

    Tweaking minor UI elements

--- main.py
+++ main.py
@@ -4,15 +4,16 @@
 import pygame
 import pygame_widgets
 
-from Visualization.settings import FPS
+from Visualization.colors import WHITE
 from Visualization.manual_visualization import ManualVisualization
-
+from Visualization.auto_visualization import AutoVisualization
+from Visualization.settings import FPS, SCREEN, FONT, BORDER
 
 if __name__ == '__main__':
     clock = pygame.time.Clock()
 
-    manual_visualization = ManualVisualization(200)
-    # auto_visualization = AutoVisualization(200, [2, 5, 5, 5, 2])
+    manual_visualization = ManualVisualization(100)
+    # auto_visualization = AutoVisualization(100, [2, 3, 3, 2])
 
     # Screen window loop
     running = True
@@ -30,6 +31,10 @@ if __name__ == '__main__':
 
         manual_visualization.draw_visualization()
         # auto_visualization.draw_visualization()
+
+        fps = str(int(clock.get_fps()))
+        text = FONT.render(f"FPS: {fps}", True, WHITE)
+        SCREEN.blit(text, [BORDER.width - 100, 10])
 
         pygame_widgets.update(events)
         pygame.display.update()
