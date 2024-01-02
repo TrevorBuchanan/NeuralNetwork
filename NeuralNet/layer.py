f2f8e049ae4bae0ae928fe78f2b50ec9bc2d2170
TrevorBuchanan <123215971+TrevorBuchanan@users.noreply.github.com>
2024-01-02 00:19:00 -0800

    Adding simple self learning logic

--- NeuralNet/layer.py
+++ NeuralNet/layer.py
@@ -1,10 +1,8 @@
-import numpy as np
 import math
 import random
 
 
 def activation_function(weighted_input):
-    # return weighted_input
     if weighted_input > 500:
         return 1
     if weighted_input < -500:
@@ -20,10 +18,22 @@ class Layer:
 
         self.weights = [[random.uniform(-1, 1) / input_amount ** 0.5 for _ in range(output_amount)] for _ in
                         range(input_amount)]
-        self.biases = [0 for _ in range(output_amount)]
+        self.wGradients = [[0 for _ in range(output_amount)] for _ in range(input_amount)]
 
-    # Compute outputs
+        self.biases = [0 for _ in range(output_amount)]
+        self.bGradients = [0 for _ in range(output_amount)]
+
+    @staticmethod
+    def node_cost(calculated, expected):
+        error = calculated - expected
+        return error * error
+
     def get_layer_outputs(self, inputs):
+        """
+        Get list of outputs after running inputs through weights and biases in layer
+        :param inputs: List (of length self.input_amount) of numbers
+        :return: List of output numbers having run through layer and activation function
+        """
         activations = [0 for _ in range(self.numOutputs)]
         for out_index in range(self.numOutputs):
             weighted_input = self.biases[out_index]
@@ -32,3 +42,13 @@ class Layer:
             activations[out_index] = activation_function(weighted_input)
 
         return activations
+
+    def apply_gradients(self, learn_rate):  # NOT FINAL
+        for out_index in range(self.numOutputs):
+            self.biases[out_index] -= self.bGradients[out_index] * learn_rate
+            for in_index in range(self.numInputs):
+                # Why set equal to
+                self.weights[in_index][out_index] -= self.wGradients[in_index][out_index] * learn_rate
+
+        self.wGradients = [[0 for _ in range(self.numOutputs)] for _ in range(self.numInputs)]
+        self.bGradients = [0 for _ in range(self.numOutputs)]
--- NeuralNet/neuralnetwork.py
+++ NeuralNet/neuralnetwork.py
@@ -43,3 +43,54 @@ class NeuralNet:
         else:
             raise Exception("Incorrectly sized input for neural network")
         return inputs
+
+    def cost(self, datapoint):
+        """
+        Get cost for a single datapoint
+        :param datapoint: Input type of DataPoint holding inputs and expected outputs
+        :return: accumulated cost from each node cost in the output layer
+        """
+        calculated_outputs = self.get_outputs(datapoint.inputs)
+        output_layer = self.layers[-1]
+        cost = 0
+        for i, calc_out in enumerate(calculated_outputs):
+            cost += output_layer.node_cost(calc_out, datapoint.expected_outputs[i])
+        return cost
+
+    # For manual network control the cost may not be an accurate depiction of how well data is classified
+    # due to the decision logic of the classify function. If outputs are [1, 0.9], and desired output is
+    # poisonous 1 or [1, 0], then there will be a total cost of 0.9 * 0.9 even though the output was "correct".
+    def total_cost(self, data):
+        """
+        Get total cost for list of DataPoints
+        :param data: List of DataPoints
+        :return: Total cost for datapoints list
+        """
+        total_cost = 0
+        for datapoint in data:
+            total_cost += self.cost(datapoint)
+        return total_cost / len(data)
+
+    def apply_all_gradients(self, learn_rate):
+        for layer in self.layers:
+            layer.apply_gradients(learn_rate)
+
+    def learn(self, training_data, learn_rate):
+        h = 0.0001
+        original_cost = self.total_cost(training_data)
+
+        for layer in self.layers:
+            for in_index in range(layer.numInputs):
+                for out_index in range(layer.numOutputs):
+                    layer.weights[in_index][out_index] += h
+                    delta_cost = self.total_cost(training_data) - original_cost
+                    layer.weights[in_index][out_index] -= h
+                    layer.wGradients[in_index][out_index] = delta_cost / h
+
+            for bias_index in range(len(layer.biases)):
+                layer.biases[bias_index] += h
+                delta_cost = self.total_cost(training_data) - original_cost
+                layer.biases[bias_index] -= h
+                layer.bGradients[bias_index] = delta_cost / h
+
+        self.apply_all_gradients(learn_rate)
